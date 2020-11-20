c0f8724d78be344283942508e36c7ef0994e5948
Alexey Soshin <alexey.soshin@gmail.com>
2020-11-20 12:17:43 +0000

    Reformat currying example

--- Chapter05/src/main/kotlin/chapter5/CurryingExample.kt
+++ Chapter05/src/main/kotlin/chapter5/CurryingExample.kt
@@ -1,24 +1,29 @@
 package chapter5
 
 
-
-fun main(args: Array<String>) {
+fun main() {
 
     println(subtract(4)(5))
 
 }
 
+// If this is a function without currying at all
 fun subtract(x: Int, y: Int): Int {
     return x - y
 }
 
-
-/*fun subtract(x: Int): (Int) -> Int {
+// This is a complex way to implement currying
+/*
+fun subtract(x: Int): (Int) -> Int {
     return fun(y: Int): Int {
         return x - y
     }
-}*/
+}
+*/
 
-fun subtract(x: Int) = {y: Int -> x - y }
-
+// And it could be simplified
 //fun subtract(x: Int) = fun(y: Int) = x - y
+
+// Simplest way
+fun subtract(x: Int) = { y: Int -> x - y }
+
